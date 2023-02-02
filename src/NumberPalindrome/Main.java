4142b743a680bc7ef2e499434956a21b934f639a
root <ring3367@naver.com>
2023-02-02 11:24:40 +0900

    # 2023-02-02-11:25

---
+++ src/NumberPalindrome/Main.java
@@ -0,0 +1,19 @@
+package NumberPalindrome;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            x *= -1;
+        }
+        String str = String.valueOf(x);
+        StringBuilder sb = new StringBuilder(str);
+
+        return sb.reverse().toString().equals(str);
+    }
+}
