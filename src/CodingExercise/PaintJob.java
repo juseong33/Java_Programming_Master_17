20be0ab3464fd4784b31c3d73e6d46bc50f42644
root <ring3367@naver.com>
2023-02-09 13:25:44 +0900

    # 2023-02-09-13:25

--- src/CodingExercise/PaintJob.java
+++ src/CodingExercise/PaintJob.java
@@ -2,6 +2,49 @@ package CodingExercise;
 
 public class PaintJob {
     public static void main(String[] args) {
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+        System.out.println("123");
+        System.out.println(getBucketCount( + 0.0, 2.1, 1.5, 7));
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
+    }
 
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        if (extraBuckets == 0) {
+            return getBucketCount(width, height, areaPerBucket);
+        }
+
+        double area = width * height;
+        int result = (int) Math.ceil((area - (areaPerBucket * extraBuckets)) / areaPerBucket);
+        return result;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket < 0) {
+            return -1;
+        }
+
+        double area = width * height;
+        int result = (int) Math.ceil(area / areaPerBucket);
+        return result;
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        int result = (int) Math.ceil(area / areaPerBucket);
+        return result;
     }
 }
